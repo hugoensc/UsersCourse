d5aacd74eda75150239ed184bc6be5590b80e22c
Hugo Santamaria <hugo.santamaria@alianzacorp.mx>
2020-01-02 13:35:15 -0600

    Firs Commit (Enrutamiento de atributos #1)

---
+++ Users/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Users.Controllers
+{
+    [Route("/Nombre")]
+    public class UsersController : Controller
+    {
+        //[HttpGet]
+        //public IActionResult Index(int id)
+        //public IActionResult Index(String data)
+        [Route("/Nombre/Hugo")]
+        public IActionResult Index(String data, int age)
+        {
+            String datos = data + " " + age;
+            //ViewData["id"] = data + " " + age;
+            //ViewData["id"] = data;
+            return View("Index", datos);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
