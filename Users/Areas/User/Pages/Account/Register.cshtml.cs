7a1935ec4fdc4cbf5bd9b631f6ec616a90558a2a
Hugo Santamaria <hugo.santamaria@alianzacorp.mx>
2020-01-21 12:28:30 -0600

    Diseñando la estructura de la página registrar

--- Users/Areas/User/Pages/Account/Register.cshtml.cs
+++ Users/Areas/User/Pages/Account/Register.cshtml.cs
@@ -4,15 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace Users.Areas.User.Pages.Account
 {
     public class RegisterModel : PageModel
     {
-        public String Message { get; set; }
-        public void OnGet(String data)
+        //public String Message { get; set; }
+        //public void OnGet(String data)
+        public void OnGet()
         {
-            Message = data;
+            //Message = data;
+        }
+        
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public String Email { get; set; }
         }
     }
 }
