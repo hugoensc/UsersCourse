3cab54a0595aa63d774dd7b73968fda1cf0d480c
Hugo Santamaria <hugo.santamaria@alianzacorp.mx>
2020-01-23 17:54:06 -0600

    Creando procedimientos para registrar usuarios

--- Users/Areas/User/Pages/Account/Register.cshtml.cs
+++ Users/Areas/User/Pages/Account/Register.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace Users.Areas.User.Pages.Account
 {
@@ -12,6 +13,14 @@ namespace Users.Areas.User.Pages.Account
     {
         //public String Message { get; set; }
         //public void OnGet(String data)
+
+        private UserManager<IdentityUser> _userManager;
+
+        public RegisterModel(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public void OnGet()
         {
             //Message = data;
@@ -43,11 +52,23 @@ namespace Users.Areas.User.Pages.Account
             public String ErrorMessage { get; set; }
         }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPost()
         {
             if (ModelState.IsValid)
             {
+                var userList = _userManager.Users.Where(u => u.Email.Equals(Input.Email)).ToList();
+                if (userList.Count.Equals(0))
+                {
+                    var result = await _userManager.CreateAsync(user, Input.Password);
+                }
+                else
+                {
+                    Input = new InputModel
+                    {
+                        ErrorMessage = "El " + Input.Email + " ya esta registrado",
+                    };
 
+                }
             }
             //else
             //{
