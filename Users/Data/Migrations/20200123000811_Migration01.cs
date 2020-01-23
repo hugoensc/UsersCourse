d2f2433ca5e14289df725c4abb5ae39e9c7118b9
Hugo Santamaria <hugo.santamaria@alianzacorp.mx>
2020-01-23 10:12:15 -0600

    Creando la base de datos en el servidor y realizando la migración

---
+++ Users/Data/Migrations/20200123000811_Migration01.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Users.Data.Migrations
+{
+    public partial class Migration01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
